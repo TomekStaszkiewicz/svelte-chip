24c2632180560ea65280abe0a60de2e3e058c881
tomekstaszkiewicz <staszkiewicztoma@gmail.com>
2022-06-21 00:04:38 +0200

    Create basic examples

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,53 @@
 
 <script lang="ts">
 	import SvelteChip from '$lib/SvelteChip.svelte';
+	import { addChip, chipsStore } from '$lib/store';
+	import type { IChip } from '$lib/types';
+
+	let currentValue = '';
+	let chips: IChip[] = [];
+	chipsStore.subscribe((newChips) => {
+		chips = newChips;
+	});
+
+	const onChangeInput = (e) => {
+		currentValue = e.target.value + ' ';
+	}
+
+	const onAddChip = () => {
+		addChip(currentValue);
+		currentValue = '';
+	}
 </script>
 
 <section>
+	<h2>Basic usage</h2>
 	<SvelteChip />
-	<SvelteChip id="another" />
-	<SvelteChip id="more" />
+</section>
 
 
+<section>
+	<h2>Custom input renderer</h2>
+	<SvelteChip>
+		<input slot="input" bind:value={currentValue} on:input={onChangeInput} on:change={onAddChip} />
+	</SvelteChip>
+</section>
+
+
+<section>
+	<h2>Custom chip renderer</h2>
+	<SvelteChip>
+		<table slot="chips">
+			<tr>
+				<th>Tag</th>
+				<th>Color</th>
+			</tr>
+			{#each chips as chip}
+				<tr>
+					<td>{chip.tag}</td>
+					<td>{chip.bgColor}</td>
+				</tr>
+			{/each}
+		</table>
+	</SvelteChip>
 </section>
